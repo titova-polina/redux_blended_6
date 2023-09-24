4e4c637fbe844a2ade1b427db1b81fe3b1f24f52
Vovap1991 <123987929+Vovap1991@users.noreply.github.com>
2023-09-24 15:47:18 +0300

    task 2 done

---
+++ src/redux/store.js
@@ -0,0 +1,33 @@
+import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { todosReducer } from './todoSlice';
+
+const persistConfig = {
+  key: 'todos',
+  version: 1,
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, todosReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
